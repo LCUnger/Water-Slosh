a0e1838c419351e7185cdc642f6975b313beba5a
Luuk Unger <l.c.unger@student.tudelft.nl>
2026-05-01 21:21:25 +0200

    added particle class

---
+++ Particle.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Point2.h"
+#include "Vec2.h"
+#include "Point2Vec2Ops.h"
+
+class Particle
+{
+public:
+	Particle() : pos(0, 0), velocity(0, 0) {}
+	Particle(float x, float y, float v_x, float v_y) : pos(x, y), velocity(v_x, v_y) {}
+
+	void update(float dt)
+	{
+		pos += velocity * dt;
+	}
+
+
+private:
+	Point2<float> pos{};
+	Vec2<float> velocity{};
+};

258be7aad4c1a504eeaad068575bec7a98d3f976
Luuk Unger <l.c.unger@student.tudelft.nl>
2026-05-02 23:09:27 +0200

    added scalarfield class

---
+++ ScalarField.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Vec.h"
+#include "Point.h"
+#include "PointVecOps.h"
+
+template<typename T, int Dimensions, typename FieldType>
+class ScalarField
+{
+public:
+	
+	
+
+	T sample(const Point<T, Dimensions>& world_position) const {
+		auto field_position = world_position - FieldType::offset<T, Dimensions>;
+
+
+	}
+
+
+private:
+};

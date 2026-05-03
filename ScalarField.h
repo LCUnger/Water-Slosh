d115d6d4680e5511996657dbf50cec4d5f4d1bdc
Luuk Unger <l.c.unger@student.tudelft.nl>
2026-05-03 19:13:36 +0200

    added helper struct to derive the DataType (read Tensor type) of the data of the field

--- ScalarField.h
+++ ScalarField.h
@@ -3,12 +3,32 @@
 #include "Vec.h"
 #include "Point.h"
 #include "PointVecOps.h"
+#include "Tensor.h"
 
 namespace toolbox
 {
-template<typename T, int dimensions, typename FieldType>
+template<typename T, std::size_t dimensions, typename FieldType, std::size_t... Extents>
 class ScalarField
 {
+	template<typename IndexSequence>
+	struct DataTypeHelper;
+
+	template<std::size_t... Is>
+	struct DataTypeHelper<std::index_sequence<Is...>>
+	{
+		using type = Tensor<T, dimensions, (Extents + static_cast<std::size_t>(FieldType::index_extend[Is]))...>;
+	};
+
+	using DataType = typename DataTypeHelper<std::make_index_sequence<dimensions>>::type;
+
+
+	// Other solution, less complicated but less elegant:
+	//template<std::size_t... Is>
+	//static Tensor<T, dimensions, (Extents + FieldType::index_extend[Is]) ...> make_data_type(std::index_sequence<Is...>);
+
+	//using DataType = decltype(make_data_type(std::make_index_sequence<dimensions>{}));
+
+
 public:
 	
 
@@ -20,7 +40,7 @@ public:
 
 
 private:
-
+	DataType data;
 
 };
 }

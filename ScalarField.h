e0336caecd68e67de6c45d9804f0829adca918d7
Luuk Unger <l.c.unger@student.tudelft.nl>
2026-05-04 19:08:05 +0200

    added transformation function between worldview and fieldview, based on the type of grid (it's offset).

--- ScalarField.h
+++ ScalarField.h
@@ -35,12 +35,12 @@ public:
 		return T{};
 	}
 
-	Point<T, dimensions> worldToField(const Point<T, dimensions>& world_position) const
+	PointType worldToField(const PointType& world_position) const
 	{
 		return world_position - FieldType::offset;
 	}
 
-	Point<T, dimensions> fieldToWorld(const Point<T, dimensions>& field_position) const
+	PointType fieldToWorld(const PointType& field_position) const
 	{
 		return field_position + FieldType::offset;
 	}

cd8946b5f67c6ae3ef7a1999cbfe98e7aeaa1f33
Luuk Unger <l.c.unger@student.tudelft.nl>
2026-05-07 17:30:23 +0200

    Created constructor

--- ParticleRenderer.h
+++ ParticleRenderer.h
@@ -1,13 +1,30 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
+
+#include <vector>
+
+#include "Particle.h"
+
+
 class ParticleRenderer
 {
 public:
-	void draw()
+	ParticleRenderer(float radius = 2.0f, float pixels_per_unit = 1.0f)
+		: radius_(radius), pixels_per_unit_(pixels_per_unit)
 	{
+		circle_.setRadius(radius_);
+		circle_.setOrigin({ radius_, radius_ });
+		circle_.setFillColor(color_);
+	}
+
 		
 	}
 
 private:
 
+	float radius_{};
+	float pixels_per_unit_{};
+	sf::Color color_ = sf::Color::Blue;
+	sf::CircleShape circle_{};
 };

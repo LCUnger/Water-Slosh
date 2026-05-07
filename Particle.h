5368d7f52ad7b6fbaca32fc675975a11c7c8f62f
Luuk Unger <l.c.unger@student.tudelft.nl>
2026-05-07 14:36:54 +0200

    implemented public acces points for position and velocity.

--- Particle.h
+++ Particle.h
@@ -11,13 +11,21 @@ public:
     Particle(float position_x, float position_y, float velocity_x, float velocity_y) : position_{ position_x, position_y }, velocity_{ velocity_x, velocity_y } {}
 
 
-    /* Integrate / update the particle's position and velocity over time dt, given an acceleration.
-    Using semi-implicit Euler integration method. */
+    /** 
+    Integrate/update the particle's position and velocity over time dt, given an acceleration.
+    Using semi-implicit Euler integration method. 
+    */
     void integrate(float dt, const Vec2& acceleration)
     {	
         velocity_ += acceleration * dt;
         position_ += velocity_ * dt;
     }
+
+	Point2& position() { return position_; }
+	const Point2& position() const { return position_; }
+
+	Vec2& velocity() { return velocity_; }
+    const Vec2& velocity() const { return velocity_; }
 
 
 private:

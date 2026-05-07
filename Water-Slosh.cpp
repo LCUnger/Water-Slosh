b13fb97634e922f9095d5dacccf7fc00feaaf275
Luuk Unger <l.c.unger@student.tudelft.nl>
2026-05-07 19:05:48 +0200

    implemented new configs

--- Water-Slosh.cpp
+++ Water-Slosh.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <cstdint>
 
+#include "ParticleRenderer.h"
 #include "World.h"
 
 struct WindowConfig
@@ -27,20 +28,17 @@ int main()
 	World world(simulation_config);
     ParticleRenderer particle_renderer(render_config);
 
-	World world(num_particles, width, height);
+    sf::RenderWindow window(sf::VideoMode({ window_config.width_px, window_config.height_px }), "SFML 3 Test Render");
 
-
-    sf::RenderWindow window(sf::VideoMode({ width, height }), "SFML 3 Test Render");
-
-    sf::Image image({ width, height }, sf::Color::Black);
+    sf::Image image({ window_config.width_px, window_config.height_px }, sf::Color::Black);
 
     // Render once, like a ray tracer frame
     for (unsigned int y = 0; y < window_config.height_px; ++y)
     {
         for (unsigned int x = 0; x < window_config.width_px; ++x)
         {
-            float texture_u = static_cast<float>(x) / static_cast<float>(width);
-            float texture_v = static_cast<float>(y) / static_cast<float>(height);
+            float texture_u = static_cast<float>(x) / static_cast<float>(window_config.width_px);
+            float texture_v = static_cast<float>(y) / static_cast<float>(window_config.height_px);
 
             std::uint8_t red = static_cast<std::uint8_t>(255 * texture_u);
             std::uint8_t green = static_cast<std::uint8_t>(255 * texture_v);
@@ -76,6 +74,7 @@ int main()
 
         window.clear();
         window.draw(sprite);
+        particle_renderer.draw(window, world.particles());
         window.display();
     }
 }

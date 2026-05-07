526079e1b4c3f4124bdad80f3483a440b9e1662c
Luuk Unger <l.c.unger@student.tudelft.nl>
2026-05-07 19:00:19 +0200

    aded simulation config

--- World.h
+++ World.h
@@ -5,6 +5,24 @@
 #include "Particle.h"
 #include "FluidGrid.h"
 
+struct SimulationConfig
+{
+	std::size_t num_particles = 1000;
+	std::size_t grid_width = 100;
+	std::size_t grid_height = 60;
+	double cell_size_m = 0.05;
+
+	double domain_width_m() const
+	{
+		return static_cast<double>(grid_width) * cell_size_m;
+	}
+
+	double domain_height_m() const
+	{
+		return static_cast<double>(grid_height) * cell_size_m;
+	}
+};
+
 class World
 {
 public:
